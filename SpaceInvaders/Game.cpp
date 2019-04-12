c0c23312b99c1d51f16eab58eea618c28a07b9b3
Hantz V <errpell@protonmail.com>
2019-04-12 13:39:36 -0400

    Updated fpga code

--- SpaceInvaders/Game.cpp
+++ SpaceInvaders/Game.cpp
@@ -1,73 +1,76 @@
 #include "Game.h"
 #include <QDebug>
 
-Game::Game(ControllerFPGA* fpga) {
-    setBackgroundBrush(QPixmap("./ressources/SpaceInvadersBg.jpg"));
-    player1 = new Ship(500, 900);
-    addSprite(player1);
-    Maison h(200, 750);
-    QList<Block*>* temp;
-    temp = h.getMaison();
-    for (QList<Block*>::iterator it = temp->begin(); it != temp->end(); ++it) {
-        addSprite(*it);
-    }
-    minions = new MinionsArray();
-    connect(minions, &MinionsArray::addSprites, this, &Display::addSprite);
-    QList<Minion*>* temp1;
-    temp1 = minions->getArray();
-    for (QList<Minion*>::iterator it = temp1->begin(); it != temp1->end(); ++it) {
-        connect((*it), &Sprites::deleteThis, minions, &MinionsArray::removeMinion);
-        addSprite(*it);
-    }
-    this->fpga = fpga;
-    timefpga = new QTimer();
-    connect(timefpga, SIGNAL(timeout()), this, SLOT(checkFPGA()));
-    timefpga->start(2);
+Game::Game(ControllerFPGA* fpga)
+{
+  setBackgroundBrush(QPixmap("./ressources/SpaceInvadersBg.jpg"));
+  player1 = new Ship(500, 900);
+  addSprite(player1);
+  Maison h(200, 750);
+  QList<Block*>* temp;
+  temp = h.getMaison();
+  for (QList<Block*>::iterator it = temp->begin(); it != temp->end(); ++it) {
+    addSprite(*it);
+  }
+  minions = new MinionsArray();
+  connect(minions, &MinionsArray::addSprites, this, &Display::addSprite);
+  QList<Minion*>* temp1;
+  temp1 = minions->getArray();
+  for (QList<Minion*>::iterator it = temp1->begin(); it != temp1->end(); ++it) {
+    connect((*it), &Sprites::deleteThis, minions, &MinionsArray::removeMinion);
+    addSprite(*it);
+  }
+  this->fpga = fpga;
+  timefpga = new QTimer();
+  connect(timefpga, SIGNAL(timeout()), this, SLOT(checkFPGA()));
+  timefpga->start(2);
 }
 
-Game::~Game() {
-    delete player1;
+Game::~Game()
+{
+  delete player1;
 }
 
-
-
-void Game::keyPressEvent(QKeyEvent* event) {
-    QList<Projectile*>* temp;
-    switch (event->key()) {
+void
+Game::keyPressEvent(QKeyEvent* event)
+{
+  QList<Projectile*>* temp;
+  switch (event->key()) {
     case Qt::Key_Left:
-        player1->move(-10);
-        break;
+      player1->move(-10);
+      break;
     case Qt::Key_Right:
-        player1->move(10);
-        break;
+      player1->move(10);
+      break;
     case Qt::Key_Space:
-        temp = player1->shoot();
-        if(temp) {
-            for (QList<Projectile*>::iterator it = temp->begin(); it != temp->end(); ++it) {
-                addSprite(*it);
-            }
+      temp = player1->shoot();
+      if (temp) {
+        for (QList<Projectile*>::iterator it = temp->begin(); it != temp->end();
+             ++it) {
+          addSprite(*it);
         }
-        
-        break;
+      }
+
+      break;
     default:
-        QWidget::keyPressEvent(event);
-    }
+      QWidget::keyPressEvent(event);
+  }
 }
 
-void Game::checkFPGA() {
-    fpga->checkVoice();
-    if (fpga->getVoiceState() & PA) {
-        //ACTION
-    }
-    if (fpga->getVoiceState() & PE) {
-        //ACTION
-    }
-    if (fpga->getVoiceState() & PI) {
-        //ACTION
-    }
-    if (fpga->getVoiceState() & PO) {
-        //ACTION
-    }
-
+void
+Game::checkFPGA()
+{
+  fpga->checkVoice();
+  if (fpga->getVoiceState() & PA) {
+    // ACTION
+  }
+  if (fpga->getVoiceState() & PE) {
+    // ACTION
+  }
+  if (fpga->getVoiceState() & PU) {
+    // ACTION
+  }
+  if (fpga->getVoiceState() & PO) {
+    // ACTION
+  }
 }
-
